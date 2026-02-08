93448e715621fccd30459a674a5f1f9a5a678d1c
Abhay Singh <abhaysingh14922@gmail.com>
2026-02-08 21:20:23 +0530

    Add files via upload

---
+++ balance_binary_tree.java
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/balanced-binary-tree/description/?envType=daily-question&envId=2026-02-08
+
+import javax.swing.tree.TreeNode;
+
+
+public class balance_binary_tree {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
